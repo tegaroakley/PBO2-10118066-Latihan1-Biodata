f97e5034cbce04f2e4e1f79de2b5ceea6bf11e5e
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 15:40:44 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan1/biodata/PBO210118066Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan1.biodata;
+
+/**
+ *
+ *
+ * @author 
+ * NAMA : Tegar Lucky'Q Oakley 
+ * NIM : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk menampilkan Biodata author
+ */
+public class PBO210118066Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "_____________________________________________");
+        System.out.println("NAMA\t\t: Tegar Lucky'Q Oakley");
+        System.out.println("ALAMAT\t\t: Jalan Jatiluhur 06 no. D244");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 21 Desember 2000");
+        System.out.println("_____________________________________________");
+        System.out.println("HOBI\t\t: Membaca dan Photograpy");
+        System.out.println("MAKES\t\t: Daging Kambing");
+        System.out.println("MIKES\t\t: Kopi Caramel Machiato");
+    }
+
+}
